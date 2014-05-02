98c9449fecbcc4e9ba841462e0a69bf002836a69
lapple <lapple@yandex-team.ru>
2014-05-02 17:13:08 +0400

    Prevent DOM-bound view invalidations. Fixes #1

--- ns-rivets-adapter.js
+++ ns-rivets-adapter.js
@@ -8,7 +8,12 @@
     }
 }(this, function (ns, rivets) {
 
-    var View = ns.View;
+    var ATTRIBUTE_ADAPTER = ':';
+    var ITERATION_ADAPTER = '*';
+
+    var CHANGED_EVENT = 'ns-model-changed';
+    var INSERT_EVENT = 'ns-model-insert';
+    var REMOVE_EVENT = 'ns-model-remove';
 
     /**
      * Checks if the provided object is an `ns.Model` instance.
@@ -28,6 +33,23 @@
         return object instanceof ns.ModelCollection;
     };
 
+    /**
+     * Returns jpath that is assigned to the given `:` keypath, e.g.
+     *
+     *     models.message:loading   -> .loading
+     *     models.todo:state.hidden -> .state.hidden
+     *
+     * @param  {String} keypath
+     * @return {String}
+     */
+    var getJpathFromKeypath = function(keypath) {
+        if (keypath.indexOf(ATTRIBUTE_ADAPTER) !== -1) {
+            return '.' + keypath.split(ATTRIBUTE_ADAPTER).pop();
+        }
+
+        return null;
+    };
+
     rivets.configure({
         handler: function(target, event, binding) {
             this.call(binding.model, event, binding);
@@ -37,15 +59,15 @@
     /**
      * Subscribes to model attribute update.
      */
-    rivets.adapters[':'] = {
+    rivets.adapters[ATTRIBUTE_ADAPTER] = {
         subscribe: function(obj, keypath, callback) {
             if (isModel(obj)) {
-                obj.on('ns-model-changed.' + keypath, callback);
+                obj.on(CHANGED_EVENT + '.' + keypath, callback);
             }
         },
         unsubscribe: function(obj, keypath, callback) {
             if (isModel(obj)) {
-                obj.off('ns-model-changed.' + keypath, callback);
+                obj.off(CHANGED_EVENT + '.' + keypath, callback);
             }
         },
         read: function(obj, keypath) {
@@ -63,17 +85,17 @@
     /**
      * Subscribes to collection contents update.
      */
-    rivets.adapters['*'] = {
+    rivets.adapters[ITERATION_ADAPTER] = {
         subscribe: function(obj, keypath, callback) {
             if (isCollection(obj)) {
-                obj.on('ns-model-insert', callback);
-                obj.on('ns-model-remove', callback);
+                obj.on(INSERT_EVENT, callback);
+                obj.on(REMOVE_EVENT, callback);
             }
         },
         unsubscribe: function(obj, keypath, callback) {
             if (isCollection(obj)) {
-                obj.off('ns-model-insert', callback);
-                obj.off('ns-model-remove', callback);
+                obj.off(INSERT_EVENT, callback);
+                obj.off(REMOVE_EVENT, callback);
             }
         },
         read: function(obj, keypath) {
@@ -83,11 +105,14 @@
         }
     };
 
+    var View = ns.View;
+    var ViewPrototype = View.prototype;
+
     var ViewRivets = function() {};
 
     return no.inherit(ViewRivets, View, {
         _init: function() {
-            View.prototype._init.apply(this, arguments);
+            ViewPrototype._init.apply(this, arguments);
 
             this.on('ns-view-htmlinit', this._bindRivetsView);
             this.on('ns-view-htmldestroy', this._unbindRivetsView);
@@ -100,6 +125,60 @@
         },
         _unbindRivetsView: function() {
             this._rivetsView.unbind();
+        },
+        invalidate: function(eventName, data) {
+            var _invalidate = ViewPrototype.invalidate;
+
+            /**
+             * Checks whether given binding synchronizes DOM and model.
+             * TODO: Check for the model, now checks only for keypath
+             * matching.
+             * @param  {Binding} binding
+             * @return {Boolean}
+             */
+            var isMatchingBinding = function(binding) {
+                // For `ns-model-changed` event, the second argument is
+                // either the jpath that corresponds to the updated
+                // attribute, or, in case of `ModelCollection`, an object
+                // containing the information about updated child model.
+                if (eventName === CHANGED_EVENT) {
+                    var boundJpath = getJpathFromKeypath(binding.keypath);
+                    var found = false;
+
+                    if (typeof data === 'string') {
+                        found = (boundJpath === data);
+                    } else {
+                        found = (boundJpath === data.jpath);
+                    }
+
+                    if (found) {
+                        return true;
+                    }
+
+                    if (binding.iterated) {
+                        return binding.iterated.some(function(view) {
+                            return view.bindings.some(isMatchingBinding);
+                        });
+                    }
+                }
+
+                if (eventName === INSERT_EVENT || eventName === REMOVE_EVENT) {
+                    return binding.keypath.indexOf(ITERATION_ADAPTER) !== -1;
+                }
+            };
+
+            if (typeof eventName === 'string') {
+                if (this._rivetsView.bindings.some(isMatchingBinding)) {
+                    this.keepValid();
+                } else {
+                    _invalidate.apply(this, arguments);
+                }
+
+            // Unconditionally invalidate this view, when it is not invoked
+            // as a model attribute change handler.
+            } else {
+                _invalidate.apply(this, arguments);
+            }
         }
     });
 
